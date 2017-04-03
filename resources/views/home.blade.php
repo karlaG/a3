aaeec12f3b2b1bc4a7667cfb136cca7c11fca96f
Karla Guardado <kmg023@g.harvard.edu>
2017-04-03 12:14:17 -0400

    Style changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,35 +2,44 @@
 
 @section('content')
   <form method="GET" action="/submit">
-    <p>I am</p>
-    <input type="radio" name="gender" value="female" {{ ($gender=='female') ? 'CHECKED': '' }} required> Female <br>
-    <input type="radio" name="gender" value="male" {{ ($gender=='male') ? 'CHECKED': '' }} required> Male <br>
+    <div class="form-group">
+      <p>*I am</p>
+      <input type="radio" name="gender" value="female" {{ ($gender=='female') ? 'CHECKED': '' }} required> Female <br>
+      <input type="radio" name="gender" value="male" {{ ($gender=='male') ? 'CHECKED': '' }} required> Male <br>
+    </div>
 
     <p>and my birthdate is</p>
 
-    <label for="month">Month</label>
-    <select name="month" required>
-      <option value="">...</option>
-      <option value="1" {{ ($month=='1') ? 'SELECTED': '' }}>Jan</option>
-      <option value="2" {{ ($month=='2') ? 'SELECTED': '' }}>Feb</option>
-      <option value="3" {{ ($month=='3') ? 'SELECTED': '' }}>Mar</option>
-      <option value="4" {{ ($month=='4') ? 'SELECTED': '' }}>Apr</option>
-      <option value="5" {{ ($month=='5') ? 'SELECTED': '' }}>May</option>
-      <option value="6" {{ ($month=='6') ? 'SELECTED': '' }}>Jun</option>
-      <option value="7" {{ ($month=='7') ? 'SELECTED': '' }}>Jul</option>
-      <option value="8" {{ ($month=='8') ? 'SELECTED': '' }}>Aug</option>
-      <option value="9" {{ ($month=='9') ? 'SELECTED': '' }}>Sep</option>
-      <option value="10" {{ ($month=='10') ? 'SELECTED': '' }}>Oct</option>
-      <option value="11" {{ ($month=='11') ? 'SELECTED': '' }}>Nov</option>
-      <option value="12" {{ ($month=='12') ? 'SELECTED': '' }}>Dec</option>
-    </select>
+    <div class="form-group">
+      <label for="month">*Month</label>
+        <select name="month" required>
+          <option value="">...</option>
+          <option value="1" {{ ($month=='1') ? 'SELECTED': '' }}>Jan</option>
+          <option value="2" {{ ($month=='2') ? 'SELECTED': '' }}>Feb</option>
+          <option value="3" {{ ($month=='3') ? 'SELECTED': '' }}>Mar</option>
+          <option value="4" {{ ($month=='4') ? 'SELECTED': '' }}>Apr</option>
+          <option value="5" {{ ($month=='5') ? 'SELECTED': '' }}>May</option>
+          <option value="6" {{ ($month=='6') ? 'SELECTED': '' }}>Jun</option>
+          <option value="7" {{ ($month=='7') ? 'SELECTED': '' }}>Jul</option>
+          <option value="8" {{ ($month=='8') ? 'SELECTED': '' }}>Aug</option>
+          <option value="9" {{ ($month=='9') ? 'SELECTED': '' }}>Sep</option>
+          <option value="10" {{ ($month=='10') ? 'SELECTED': '' }}>Oct</option>
+          <option value="11" {{ ($month=='11') ? 'SELECTED': '' }}>Nov</option>
+          <option value="12" {{ ($month=='12') ? 'SELECTED': '' }}>Dec</option>
+        </select>
+    </div>
 
-    <label for="day">Day</label>
-      <input type="number" name="day" min="1" max="31" value="{{ $day or ''}}" required>
-    <br>
-    <input type="submit" value="submit">
-    <br>
-    <p>* denotes a required field.</p>
+    <div class="form-group">
+      <label for="day">*Day</label>
+        <input type="number" name="day" min="1" max="31" value="{{ $day or ''}}" required>
+    </div>
+
+    <div class="form-group">
+      <input class="btn btn-default" type="submit" value="submit">
+    </div>
+    <div class="form-group">
+      <p>* denotes a required field.</p>
+    </div>
   </form>
 
   @if (count($errors) > 0)
@@ -48,6 +57,5 @@
   @endif
 
   {{ $sign }}
-  {{ $summary }}
-
+  <div>{{ $summary }}</div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,15 +4,16 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>
+        <link href='https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css' rel='stylesheet'>
 
         <title>What's my sign?</title>
     </head>
 
     <body>
-        <h1>My sign is</h1>
-
-        <section>@yield('content')</section>
-
-        <footer>&copy; {{ date('Y') }}</footer>
+        <div class="container">
+          <h1>What's my sign?</h1>
+          <section>@yield('content')</section>
+        </div>
     </body>
 </html>
